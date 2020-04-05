fea8bbfbfc04834698f89d252bd30534bf698627
ATUL <akgnit7@gmail.com>
2020-04-05 20:20:01 +0530

    Added Token | Post Generation will be done

--- rnBackend/models/user.js
+++ rnBackend/models/user.js
@@ -22,8 +22,40 @@ const UserSchema = new Schema({
 		required: true,
 		minlength: 8,
 		trim: true
-	}
+	},
+	tokens: [
+		{
+			access: {
+				type: String,
+			},
+			token: {
+				type: String,
+			}
+		}
+	]
 });
+
+//userSchema.statics.generateAuthToken = function() {
+//	console.log(this)
+//}
+
+UserSchema.methods.generateAuthToken = function() {
+	console.log(this)
+	const user = this;
+	const access = "auth";
+
+	const token = jwt.sign({_id: user._id.toHexString(), access }, "HDG568SDG5F5FBCB").toString();
+
+	user.tokens.push({
+		access, 
+		token
+	});
+
+	return user.save().then(() => {
+		console.log("token", token)
+		return token;
+	});
+}
 
 const User = mongoose.model('User', UserSchema);
 
